226c2069dd114e62df4ce8e1d2864c775430db4e
laprn <reg@dymcom.net>
2020-10-22 23:12:06 +0900

    update footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,17 +5,23 @@ const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
   let header
-
+  let footer
   header = (
     <h1 className="main-heading">
       <Link to="/">{title}</Link>
     </h1>
   )
-
+  
+  footer = (
+    <h3 className="main-footer">
+      <Link to="/">{title}</Link>
+    </h3>
+  )
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       <header className="global-header">{header}</header>
       <main>{children}</main>
+      <footer>{footer}</footer>
     </div>
   )
 }
